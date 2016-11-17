f967c144d30dcbefffc2dd7c843413cf6266588a
Lucas Scarlato Astur <lucassa3@al.insper.edu.br>
2016-11-17 18:15:20 -0200

    saving 30 images array on profile

--- src/mvc/controller/SignupController.java
+++ src/mvc/controller/SignupController.java
@@ -32,7 +32,7 @@ public class SignupController {
 		return "signup";
 	}
 	
-	@RequestMapping("webcam-test")
+	@RequestMapping("/webcam-test")
 	public String webcam() {
 		return "webcam-test";
 	}
